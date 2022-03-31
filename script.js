184768d73698e01d497b21431102a251d76a3d41
Amalie Von Behren <vbamalie@gmail.com>
2022-03-31 18:28:41 -0400

    trying to figure out the random

--- script.js
+++ script.js
@@ -10,15 +10,15 @@ const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "
 psArr = []
 let passwordlen
 let addArr
+let pS
 
 //function to get the password options selected by the user
 //it then goes into the objects of answerObj
 function generatePassword(){
-   passwordLen = window.prompt("Pick an amount of characters between from 8-129.");
-  if (passwordLen < 8 || passwordLen > 129){
-    window.alert("Invalid answer.");
-    userSelected();
-  }
+   passwordLen = prompt("Pick an amount of characters between from 8-129.");
+  if (passwordlen < 8 || passwordlen > 129){
+    alert("Invalid answer.");
+   }
   else 
      addArr = window.confirm("Do you want Special Characters?");
       if(addArr){
@@ -36,23 +36,27 @@ function generatePassword(){
     if(confirm){
       psArr = psArr.concat(uppercase);
     };
-    return
-}
-console.log(psArr)
-generatePassword()
-console.log(psArr)
+    console.log(psArr)
+    for(i = 0; i< 8; i++){
+
+      let randomValue = psArr[Math.floor(Math.random() * psArr.length)];
+      let addChar = psArr[randomValue]
+      pS = pS + addChar;
+      console.log(pS)
+      //let random = math.floor(math.random() * psArr);
+        //let addNum = psArr[random]
+        //password += addNum
+        }
+    
+
+     }
+    
+
+
 
 
 //need an array to randomize the elements selected
 //will eventually be the concatonized version of all the arrays the user selected
-function randomSelected( psLen , randomizedArr ){
-     for (let i = 0; i < psLen; i++); {
-      let random = Math.floor(Math.random() * randomizedArr.length);
-      let newItem = randomizedArr[random]
-      password = password + newItem;
-     }
-  console.log(password)
-}
 
 //function to generate the password from the user input
 //this function needs to contatonate the lists that the user wants to be included together and then bring it back up to the randomSelected function
