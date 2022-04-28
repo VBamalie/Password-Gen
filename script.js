50d5b82cf7192aa296bc84448aaf5509ca643545
Amalie Von Behren <vbamalie@gmail.com>
2022-04-28 15:26:33 -0400

    works now

--- script.js
+++ script.js
@@ -7,50 +7,57 @@ const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "
 
 const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 //answerObj might be able to be more localized
-psArr = []
-let addArr
-let pS
-let password = ""
+
+
+
 
 //function to get the password options selected by the user
 //it then goes into the objects of answerObj
-function generatePassword(){
-   let passwordlen= prompt("Pick an amount of characters between from 8-129.");
+function passwordLengthQuestion(){
+    let passwordlen = parseInt(prompt("Pick an amount of characters between from 8-129."));
   if (passwordlen < 8 || passwordlen > 129){
-    alert("Invalid answer.");
-   }
-  else 
-     addArr = window.confirm("Do you want Special Characters?");
-      if(addArr){
-        psArr = psArr.concat(specialChars)
-      };
-    addArr = window.confirm("Do you want Numbers?");
-      if(addArr){
-        psArr = psArr.concat(numberChars)
-      };
-    addArr = window.confirm("Do you want lowercase letters?");
-    if(addArr){
-      psArr = psArr.concat(lowercase)
-    };
-    addArr = window.confirm("Do you want Uppercase letters?");
+    alert("Invalid answer.");}
+  return passwordlen
+  }
+
+
+function questions(){   
+  let psArr =[]
+  window.confirm("Do you want Special Characters?");
     if(confirm){
-      psArr = psArr.concat(uppercase);
+    psArr = psArr.concat(specialChars)
+     };
+  window.confirm("Do you want Numbers?");
+    if(confirm){
+    psArr = psArr.concat(numberChars)
     };
-    console.log(typeof passwordlen)
-    console.log(psArr)
-    for(i = 0; i< passwordlen; i++);{
-      let random = Math.floor(Math.random() * psArr.length);
-        let addNum = psArr[random]
-        password += addNum
-        console.log(password)
+  window.confirm("Do you want lowercase letters?");
+    if(confirm){
+    psArr = psArr.concat(lowercase)
+    };
+  window.confirm("Do you want Uppercase letters?");
+    if(confirm){
+    psArr = psArr.concat(uppercase);
+    };
+  return psArr
+  }
+
+
+function generatePassword(length , array){
+  var password = ""
+    for (i = 0; i < length; i++){
+      var random = Math.floor(Math.random() * array.length);
+      var addChar = array[random]
+      console.log(addChar)
+      password = password + addChar
+      console.log(password)      
+      // let addNum = psArr[random]
+        // password += addNum
+        // console.log(password)
         }
-        return
+      return password
      }
     
-
-
-
-
 //need an array to randomize the elements selected 
 //will eventually be the concatonized version of all the arrays the user selected
 
@@ -58,17 +65,15 @@ function generatePassword(){
 //this function needs to contatonate the lists that the user wants to be included together and then bring it back up to the randomSelected function
 //FIGURE OUT WHETHER THE CONCAT ARRAY NEEDS TO BE LOCAL OR GLOBAL
 
-
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
- password = generatePassword();
+ 
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = generatePassword(passwordLengthQuestion(), questions());
 
 }
 
